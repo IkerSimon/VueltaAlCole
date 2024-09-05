fab1118a1323df101293b615e9c444ca61d7c7c2
IkerSimon <76107560+IkerSimon@users.noreply.github.com>
2024-09-05 10:30:38 +0200

    Comentado Categoria
    He comentado al menos gran parte de Categoria

--- src/main/java/com/ipartek/modelo/dto/Categoria.java
+++ src/main/java/com/ipartek/modelo/dto/Categoria.java
@@ -1,44 +1,65 @@
 package com.ipartek.modelo.dto;
 
+/**
+ * <b>Clase categoria</b>
+ * 
+ * <p>Clase Categoria que referencia la tabla Categoria en la BD<p>
+ * 
+ * @author Iker
+ * 
+ */
+
 public class Categoria {
-private int id;
-private String nombre;
+	/**
+	 * Atributo ID: 
+	 * <b>A la hora de insertar en la bd es AUTOUMERIC </b>
+	 */
 
+	private int id;
+	
+	/**
+	 * Atributo nombre: 
+	 * <b>Varchar(45) en la bd </b>
+	 */
+	private String nombre;
 
-public Categoria(int id, String nombre) {
-	super();
-	this.id = id;
-	this.nombre = nombre;
-}
+	
+	/**
+	 * Constructor completo 
+	 * @param id un integer, si no lo hemos recogide de la BD no deberia ser 0 
+	 * @param nombre 
+	 */
+	public Categoria(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
 
-public Categoria() {
-	super();
-	this.id = 0;
-	this.nombre = "";
-}
+	public Categoria() {
+		super();
+		this.id = 0;
+		this.nombre = "";
+	}
 
-public int getId() {
-	return id;
-}
+	public int getId() {
+		return id;
+	}
 
-public void setId(int id) {
-	this.id = id;
-}
+	public void setId(int id) {
+		this.id = id;
+	}
 
-public String getNombre() {
-	return nombre;
-}
+	public String getNombre() {
+		return nombre;
+	}
 
-public void setNombre(String nombre) {
-	this.nombre = nombre;
-}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-@Override
-public String toString() {
-	return "Categoria [id=" + id + ", nombre=" + nombre + "]";
-}
-
-
-
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nombre=" + nombre + "]";
+	}
 
 }
